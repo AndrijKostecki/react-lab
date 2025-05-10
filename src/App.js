f68bb9e71aaf49bc51517c1f97a6dac6f11a4758
Andrij Kostecki <kostecki.andrij@gmail.com>
2025-05-10 14:42:03 +0200

    Message

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'kostecki.andrij@gmail.com';
+    //let email = 'kostecki.andrij@gmail.com';
+    const[email, setEmail] = useState('');
+    let message;
+
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value);
+
     }
+
+    if (email.length <= 5) {
+        message='za krótki';
+    } else if (email.length < 15) {
+        message='w sam raz';}
+    else if (email.length > 15) {message='długi';}
+
+
     return (
     <div >
       <h1>System do zapisów na zajęcia</h1>
         <h2>Twój-email to {email}</h2>
+
+        <div>{message}</div>
+
+
         <input type = "text" onChange={handleChange}/>
+
+
     </div>
   );
 }
